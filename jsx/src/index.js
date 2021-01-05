643f2dcbbbd94905f37d89c46be7e98b1f62b581
glaizacano <glaizacn@gmail.com>
2021-01-05 03:49:47 -0800

    Convert HTML to JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,8 +4,28 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hello World!</div>
-}
+    const titleText = 'Converting HTML to JSX';
+    const labelText = 'Enter name:';
+    const buttonText = { text: 'Click me' };
+
+    return (
+        <div>
+            <h1>
+                {titleText}
+            </h1>
+            <label htmlFor="name" className="formLabel">
+                {labelText}
+            </label>
+            <br />
+            <input type="text" id="name" />
+            <div>
+                <button type="submit" style={{backgroundColor:'black', color:'white'}}>
+                    {buttonText.text}
+                </button>
+            </div>
+        </div>
+    );
+};
 
 // Take the react component and show it on the screen
 ReactDOM.render(
