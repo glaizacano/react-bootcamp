2248fdb922c9b481fdcd2ea194dbecb86672c55d
Glaiza Cano <glaizacn@gmail.com>
2021-02-01 16:58:11 +1100

    Add seasons project

---
+++ seasons/src/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Spinner = (props) => {
+    return (
+        <div className="ui active dimmer">
+            <div className="ui massive text loader">{props.message}</div>
+        </div>
+    );
+};
+
+// sets default prop for message
+Spinner.defaultProps = {
+    message: "Loading..."
+};
+
+export default Spinner;
